b242a6ccaa3505d4b796d475cd30f2da70933d63
KristinaSafaralieva <kristinatichankova@gmail.com>
2023-03-07 17:45:54 +0400

    Removed unused code and rebuild project

--- JavaScript/burger.js
+++ JavaScript/burger.js
@@ -4,10 +4,8 @@ const sidebarBtnClose = document.querySelector('.sidebar_btn-close');
 
 sidebarBtnOpen.addEventListener('click', () => {
     sidebar.classList.add("sidebar_menu_active");
-    sidebarBtn.classList.add('sidebar_btn-close');
 });
 
 sidebarBtnClose.addEventListener('click', () => {
     sidebar.classList.remove("sidebar_menu_active");
-    sidebarBtn.classList.remove('sidebar_btn-close');
 });
